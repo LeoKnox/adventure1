86ded5e82aa7cf0de00c97863294af458332af01
LeoKnox <LeoKnox@users.noreply.github.com>
2019-03-14 22:36:58 -0700

    Add files via upload
    Added more fields for monsters. Routing to modify data for combat.

--- server.js
+++ server.js
@@ -7,29 +7,36 @@ app.use (bodyParser.urlencoded({ extended: true }))
 var path = require('path')
 mongoose.connect('mongodb://localhost/basic_mongoose')
 
-var UserSchema = new mongoose.Schema({
+var monsterSchema = new mongoose.Schema({
     name: String,
-    age: Number
+    hp: Number,
+    atk: Number,
+    def: Number,
+    mgc: Number
 })
-mongoose.model('User', UserSchema)
+mongoose.model('Monster', monsterSchema)
 mongoose.Promise = global.Promise
-var User = mongoose.model('User')
+var Monster = mongoose.model('Monster')
 
 app.use(express.static(path.join(__dirname, './static')))
 app.set('views', path.join(__dirname, './views'))
 app.set('view engine', 'ejs')
+
 app.get('/', function(req, res) {
     var adv_data = [
-        {atk: "18", def: "14", hp: "25", mgc: "18"},
-        {atk: "14", def: "17", hp: "15", mgc: "38"},
-        {atk: "20", def: "12", hp: "28", mgc: "24"},
+        {id: 1, atk: 18, def: 14, hp: 25, mgc: 18},
+        {id: 2, atk: 14, def: 17, hp: 15, mgc: 38},
+        {id: 3, atk: 20, def: 12, hp: 28, mgc: 24},
     ]
     res.render('index', {stats: adv_data});
 })
+
 app.post('/users', function(req, res) {
-    console.log("poast data", req.body)
-    var user = new User({name: req.body.name, age: req.body.age})
-    user.save(function(err) {
+    console.log("post data", req.body)
+    var monster = new Monster({name: req.body.name, hp: req.body.hp, def: req.body.def, atk: req.body.atk, mgc: req.body.mgc})
+    var x = {hp: req.body.hp-3, atk: req.body.atk};
+    console.log(x.hp);
+    monster.save(function(err) {
         if (err) {
             console.log('something went wrong')
         } else {
@@ -37,6 +44,11 @@ app.post('/users', function(req, res) {
         }
         res.redirect('/')
     })
+})
+
+app.get('/attack', function(req, res) {
+    console.log('I wIn');
+    res.redirect('/');
 })
 
 app.listen(8000, function() {
