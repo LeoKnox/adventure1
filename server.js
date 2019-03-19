cebf89e564aeca82c0929494b712b350c9e9ca6e
LeoKnox <LeoKnox@users.noreply.github.com>
2019-03-19 00:43:07 -0700

    Add files via upload
    comparison of different damages.

--- server.js
+++ server.js
@@ -33,25 +33,38 @@ app.get('/', function(req, res) {
     res.render('index', {stats: adv_data, points: req.session.chart, hp: req.session.hp})
 })
 
+app.get('/stat', function(req, res) {
+    var adv_data = [
+        {id: 1, atk: 18, def: 14, hp: 25, mgc: 18},
+        {id: 2, atk: 14, def: 17, hp: 15, mgc: 38},
+        {id: 3, atk: 20, def: 12, hp: 28, mgc: 24},
+    ]
+    res.render('index', {stats: adv_data, points: req.session.chart, hp: req.session.hp})
+
+})
+
 app.post('/users', function(req, res) {
+    console.log(req.body);
     var monster = new Monster({name: req.body.name, hp: req.body.hp, def: req.body.def, atk: req.body.atk, mgc: req.body.mgc})
     var chart = [];
     hp = [];
     var x = {hp: req.body.hp-3, atk: req.body.atk};
     while (req.body.hp > 0) {
-        var dmg = req.body.atk*Math.floor(Math.random()*req.body.fac1+1);
+        var dmf = req.body.atk*Math.floor(Math.random()*req.body.fac1+1)
+        var dmg = req.body.atk*Math.floor(Math.random() * req.body.fac1+1)-(req.body.atk-5)*(Math.random()*5+1)
         req.body.hp -= dmg;
-        hp.push(req.body.hp)
+        hp.push(dmf)
         chart.push(dmg)
     }
+    console.log(chart)
     req.session.chart = chart
     req.session.hp = hp
-    res.redirect('/')
+    res.redirect('/stat')
 })
 
 app.get('/attack', function(req, res) {
     console.log('I wIn');
-    res.redirect('/');
+    res.redirect('/stat');
 })
 
 app.listen(8000, function() {
